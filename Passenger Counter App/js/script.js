60c92cc38ca547e7139b67e102bcf3038c1777b7
Julie Viktoria <julieviktoria1@hotmail.com>
2021-10-08 16:34:28 +0200

    :boom: update count function

--- Passenger Counter App/js/script.js
+++ Passenger Counter App/js/script.js
@@ -24,6 +24,14 @@
 
 //console.log(bonusPoints)
 
+let countEl = document.getElementById('count-el')
+
+console.log(countEl)
+
+let count = 0 
+
 function increment() {
-    console.log('The button was clicked')
+    count = count + 1
+    countEl.innerText = count
+    
 }
