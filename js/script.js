8b5fe64766b5406eda3ff79d16ae1c316572cddf
Julie Viktoria <julieviktoria1@hotmail.com>
2021-10-08 15:09:30 +0200

    :construction: init

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// build a counter app
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+// let myAge = 35
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+// let bonusPoints = 50
+
+// bonusPoints = bonusPoints + 50
+
+//bonusPoints = bonusPoints - 75
+
+// bonusPoints = bonusPoints + 45
+
+//console.log(bonusPoints)
+
+function increment() {
+    console.log('The button was clicked')
+}
